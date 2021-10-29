803b9910ae8e6f4f486910af2c753a27254498a2
KaziShohiduzzaman <kazishohiduzzaman786@gmail.com>
2021-10-30 02:45:12 +0600

    onek koster foshol

--- src/components/Home/Booking/Booking.js
+++ src/components/Home/Booking/Booking.js
@@ -10,16 +10,42 @@ const Booking = () => {
     const { user } = useAuth();
     const { serviceId } = useParams();
 
+    console.log(event);
+
     useEffect(() => {
         const url = `http://localhost:5000/events/${serviceId}`;
         fetch(url)
             .then(res => res.json())
             .then(data => setEvent(data))
     }, [serviceId])
+    const preloadValues = {
+        name: user?.displayName,
+        email: user?.email,
+        // place: event?.name,
+    }
 
-
-    const { register, handleSubmit } = useForm();
-    const onSubmit = data => console.log(data);
+    const { register, handleSubmit, reset } = useForm({
+        defaultValues: preloadValues
+    });
+    const onSubmit = data => {
+        // const pname = event.name;
+        data.tourPlace = event.name;
+        data.status = "pending";
+        fetch('http://localhost:5000/orders', {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        })
+            .then(res => res.json())
+            .then(result => {
+                if (result.insertedId) {
+                    alert("Registration successful");
+                    reset();
+                }
+            })
+    };
     return (
         <div className='container'>
             <h1 className='text-center p-4 text-color-services'>{event.name}</h1>
@@ -36,9 +62,18 @@ const Booking = () => {
                         <div>
                             <h3 className='text-center p-4 text-color-services'>Registration</h3>
                             <form onSubmit={handleSubmit(onSubmit)}>
-                                <input className='input-place' defaultValue={user.displayName} {...register("name", { required: true, maxLength: 20 })} placeholder='Name' />
-                                <input className='input-place' defaultValue={user.email} {...register("email")} placeholder='Email' />
-                                <input className='input-place' defaultValue={event.name} {...register("place")} placeholder='Visiting Place' />
+                                {
+                                    user?.email ?
+                                        <input className='input-place' {...register("name", { required: true, maxLength: 20 })} placeholder='Name' value={user?.displayName || ''} />
+                                        :
+                                        <input className='input-place' {...register("name", { required: true, maxLength: 20 })} placeholder='Name' defaultValue="" />
+                                }
+                                {
+                                    user?.email ?
+                                        <input className='input-place' {...register("email")} placeholder='Email' value={user?.email || ''} />
+                                        :
+                                        <input className='input-place' {...register("email")} placeholder='Email' defaultValue="" />
+                                }
                                 <input className='input-place' defaultValue="" type="number" {...register("totalPerson")} placeholder='Total Person' />
                                 <input className='input-place' defaultValue="" type="number" {...register("phoneNumber")} placeholder='Phone No' />
                                 <input className='input-place' defaultValue="" type="number" {...register("duration")} placeholder='Travel Duration' />
